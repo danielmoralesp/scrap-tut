5d87966fd5255fb4aa18f00070eea90c5477763b
danielmoralesp <daniel_moralesp@hotmail.com>
2017-10-20 09:52:43 -0500

    Scrapp total page el tiempo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def scrape_tiempo
+    require 'open-uri'
+    tiempo = Nokogiri::HTML(open("http://www.eltiempo.com/"))
+    render text: tiempo
+  end
 end
